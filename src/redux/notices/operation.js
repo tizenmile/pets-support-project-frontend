b2346779c8fa0acd12deb91041a8594cec3cb453
tizenmile <tizen48@gmail.com>
2023-02-20 18:40:14 +0200

    Update operation.js

--- src/redux/notices/operation.js
+++ src/redux/notices/operation.js
@@ -1,8 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://pet.tizenmile.keenetic.pro/api';
-axios.defaults.baseURL = 'http://localhost:3002/api';
+axios.defaults.baseURL = 'https://pet.tizenmile.keenetic.pro/api';
+//axios.defaults.baseURL = 'http://localhost:3002/api';
 axios.defaults.headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2YwOWRkMWUwNjhhYjQ3MzRiMjIxMTciLCJpYXQiOjE2NzY5MDkxNDgsImV4cCI6MTY3Njk0NTE0OH0.HXO04D-FixgI3Ujb4DKEmLRl8WJzaJwSaRLzjtliWRY"};
 
 
