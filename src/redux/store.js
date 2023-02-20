88169f2bb67f5818f4d754e89f9bb89ce95d3986
tizenmile <tizen48@gmail.com>
2023-02-20 19:32:03 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { newsReducer } from "./news/newsSlice";
 import { noticesReducer } from "./notices/noticesSlice";
+import { authReducer } from "./auth/authSlice";
 
 export const store = configureStore({
     reducer: {
