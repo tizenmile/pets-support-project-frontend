15be60f0df90a6b6e1f27776510504a4093892d7
tizenmile <tizen48@gmail.com>
2023-02-25 20:35:42 +0200

    Update api.js

--- src/API/api.js
+++ src/API/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3002/api/static/friends";
+const BASE_URL = "https://pet.tizenmile.keenetic.pro/api/static/friends";
 //const BASE_URL = "http://localhost:3000/api/contacts";
 export const getFrends = async () => {
   const response = await axios.get(BASE_URL);
