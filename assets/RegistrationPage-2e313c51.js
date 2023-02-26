f80cd28c5838658ba0bd847f91a0e553d2297ecf
tizenmile <tizenmile@users.noreply.github.com>
2023-02-26 11:50:38 +0000

    deploy: f85aa7b25f74f58a40433d998c73c4fd83a6e4f9

--- assets/RegistrationPage-2e313c51.js
+++
@@ -1,1 +0,0 @@
-import{j as i,a}from"./index-e6188c2c.js";import{R as e,a as t,b as r,A as n,c as o,d as s}from"./AuthForm-2c837fb7.js";import"./index.esm-b3600b58.js";const d=()=>i(e,{children:a(t,{children:[i(r,{children:"Registration"}),i(n,{}),a(o,{children:["Already have an account?",i(s,{to:"/login",children:"Login"})]})]})});export{d as default};
