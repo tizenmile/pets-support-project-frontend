272283b54d42c560288e9108ad4532fd4dbde423
VladOlevskiy <99341001+VladOlevskiy@users.noreply.github.com>
2023-02-27 22:05:22 +0200

    notices fix styles

--- src/components/Notices/NoticesCategoriesNav/NoticesCategoriesNav-styled.jsx
+++ src/components/Notices/NoticesCategoriesNav/NoticesCategoriesNav-styled.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Section = styled.section``;
 
 export const Wrapper = styled.div`
-  @media (${(p) => p.theme.media.desktop}) {
+  @media ${(p) => p.theme.media.desktop} {
     margin-right: auto;
   }
 `;
@@ -17,12 +17,12 @@ export const Item = styled.li`
   &:not(:last-child) {
     margin-right: 12px;
   }
-  @media (${(p) => p.theme.media.mobileMax}) {
+  @media ${(p) => p.theme.media.mobileMax} {
     &:nth-child(-n + 3) {
       margin-bottom: 12px;
     }
   }
-  @media (${(p) => p.theme.media.tablet}) {
+  @media ${(p) => p.theme.media.tablet} {
     &:nth-child(-n + 3) {
       margin-bottom: 16px;
     }
@@ -30,7 +30,7 @@ export const Item = styled.li`
 `;
 
 export const NoticesCategoriesNavBox = styled.div`
-  @media (${(p) => p.theme.media.tablet}) {
+  @media ${(p) => p.theme.media.tablet} {
     display: flex;
     align-items: baseline;
     justify-content: space-between;
