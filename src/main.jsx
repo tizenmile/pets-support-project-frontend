85a8499ef40cb556e9d57cb1ee19ec28c4335890
tizenmile <tizen48@gmail.com>
2023-02-27 21:13:34 +0200

    im dump

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
-import { ToastContainer } from "react-toastify";
 import { ThemeProvider } from "styled-components";
 import { theme, GlobalStyle } from "./styles";
 import { HashRouter } from "react-router-dom";
