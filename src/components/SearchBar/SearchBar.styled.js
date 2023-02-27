dcea57089bb0b7f29ec7fab3f677cfe970bbf058
LisaLyakh <99537140+LisaLyakh@users.noreply.github.com>
2023-02-27 14:10:37 +0200

    update

--- src/components/SearchBar/SearchBar.styled.js
+++ src/components/SearchBar/SearchBar.styled.js
@@ -19,10 +19,13 @@ export const SearchForm = styled.form`
   max-width: 608px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
+  margin-bottom: 60px;
+  margin-top: 40px;
   overflow: hidden;
    @media (${p => p.theme.media.mobileMax}) {
     width: 280px;
    }
+
  
 `;
 
@@ -55,6 +58,7 @@ export const SearchFormButtonLabel = styled.span`
   clip: rect(0, 0, 0, 0);
   white-space: nowrap;
   clip-path: inset(50%);
+  
 `;
 
 export const SearchFormInput = styled.input`
@@ -70,9 +74,14 @@ export const SearchFormInput = styled.input`
   padding-top: 10px;
   padding-bottom: 10px;
   ::placeholder {
-    font: inherit;
+    font-family: "Inter"
+    font-style: medium;
+    /* font: inherit; */
     font-size: 20px;
-    line-height: 1.36;
+    align-items: left;
+    line-height: 24px;
+    letter-spacing: 4%;
+    color: rgba(83, 83, 83, 1);
   }
 
 `;
